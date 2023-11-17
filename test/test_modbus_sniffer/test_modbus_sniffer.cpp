f0cbe8435a959b6cfad6b4a0fc30f1c79d7260f7
Paul de Jong <paul.m.de.jong@gmail.com>
2023-11-17 15:24:00 +0100

    Start fake uart interface task in test

--- test/test_modbus_sniffer/test_modbus_sniffer.cpp
+++ test/test_modbus_sniffer/test_modbus_sniffer.cpp
@@ -15,8 +15,21 @@ void test_modbus_request_detector_request_function_3() {
   // Arrange
   FakeUartInterface fake_uart_interface;
   uint8_t fake_data[] = { 0x0B, 0x03, 0x04, 0xAF, 0x00, 0x1E, 0xF4, 0x79 };
-  fake_uart_interface.set_fake_data_to_return(fake_data, 8);
   ModbusRequestDetector modbus_request_detector(&fake_uart_interface);
+  FakeUartInterfaceTaskArgs args = { 
+    .uart_interface = &fake_uart_interface,
+    .delay_between_bytes_in_us = 573,
+    .data_to_return = fake_data,
+    .len_of_data_to_return = 8
+  };
+  xTaskCreatePinnedToCore(fake_uart_interface_task,
+                    "fake_uart_interface_task", // name
+                    30000,                      // stack size (in words)
+                    &args,                      // input params
+                    1,                          // priority
+                    nullptr,                    // Handle, not needed
+                    0                           // core
+  );
 
   // Act
   ModbusFrame *request_frame = modbus_request_detector.detect_request();
@@ -34,41 +47,9 @@ void test_modbus_request_detector_request_function_3() {
   TEST_ASSERT_EQUAL_UINT8(0x1E, actual_data[3]);
 }
 
-// void test_data_link_layer_req_ud2_different_l_fields(void) {
-//   // Arrange
-//   FakeUartInterface uart_interface;
-//   const uint8_t fake_data[] = { 0x68, 0x06, 0x05, 0x68, 0x08, 0xB2, 0x72, 0x03, 0x02, 0x01, 0x32, 0x16 };
-//   FakeUartInterfaceTaskArgs args = { 
-//     .uart_interface = &uart_interface,
-//     .respond_to_nth_write = 1,
-//     .delay_in_ms  = 5,
-//     .data_to_return = fake_data,
-//     .len_of_data_to_return = 12
-//   };
-//   xTaskCreatePinnedToCore(fake_uart_interface_task,
-//                     "fake_uart_interface_task", // name
-//                     30000,                      // stack size (in words)
-//                     &args,                      // input params
-//                     1,                          // priority
-//                     nullptr,                    // Handle, not needed
-//                     0                           // core
-//   );
-
-//   TestableDataLinkLayer data_link_layer(&uart_interface);
-//   data_link_layer.set_meter_is_initialized(true);
-
-//   // Act
-//   const uint8_t a = 0xB2;
-//   MbusReader::DataLinkLayer::LongFrame response_frame;
-//   const bool actual_return_value = data_link_layer.req_ud2(a, &response_frame);
-
-//   // Assert
-//   TEST_ASSERT_FALSE(actual_return_value);
-// }
-
 int runUnityTests(void) {
   UNITY_BEGIN();
-  RUN_TEST(test_modbus_request_detector_request_function_1);
+  RUN_TEST(test_modbus_request_detector_request_function_3);
   return UNITY_END();
 }
 
