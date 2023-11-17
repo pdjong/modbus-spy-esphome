441791b2fb331a69bf238b7848c989051930cf28
Paul de Jong <paul.m.de.jong@gmail.com>
2023-11-17 15:19:33 +0100

    Add getters to ModbusFrame

--- components/modbus_spy/modbus_frame.h
+++ components/modbus_spy/modbus_frame.h
@@ -22,13 +22,20 @@ class ModbusFrame {
  public:
   void set_address(uint8_t address) { this->address_ = address; }
   void set_function(uint8_t function) { this->function_ = function; }
-  void set_data(uint8_t* data) { this->data_ = data; }
+  void set_data(uint8_t* data, uint8_t length) { this->data_ = data; this->data_length_ = length; }
   void set_received_crc(uint16_t received_crc) { this->received_crc_ = received_crc; }
+
+  uint8_t get_address() const { return this->address_; }
+  uint8_t get_function() const { return this->function_; }
+  uint8_t* get_data() const { return this->data_; }
+  uint8_t get_data_length() const { return this->data_length_; }
+  uint16_t get_received_crc() const { return this->received_crc_; }
 
  protected:
   uint8_t address_;
   uint8_t function_;
   uint8_t* data_;
+  uint8_t data_length_;
   uint16_t received_crc_;
 };
 
