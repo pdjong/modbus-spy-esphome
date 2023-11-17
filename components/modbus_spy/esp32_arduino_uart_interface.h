5464f1e5ade3fece14e0753f73cfd94896e19ad0
Paul de Jong <paul.m.de.jong@gmail.com>
2023-11-17 14:29:20 +0100

    Add UART interface + implementation class

---
+++ components/modbus_spy/esp32_arduino_uart_interface.h
@@ -0,0 +1,39 @@
+#ifndef ESP32_ARDUINO_UART_INTERFACE_H_
+#define ESP32_ARDUINO_UART_INTERFACE_H_
+
+#include "esphome/core/datatypes.h"
+#include "esphome/components/uart/uart.h"
+
+#include "uart_interface.h"
+
+namespace esphome {
+namespace modbus_spy {
+
+class Esp32ArduinoUartInterface : public IUartInterface {
+ public:
+  Esp32ArduinoUartInterface(uart::UARTDevice* uart_device) : uart_device_(uart_device) {}
+
+  virtual bool read_byte(uint8_t* data) {
+    return this->uart_device_->read_byte(data);
+  }
+
+  virtual bool read_array(uint8_t* data, size_t len) {
+    return this->uart_device_->read_array(data, len);
+  }
+
+  virtual void write_array(const uint8_t* data, size_t len) {
+    this->uart_device_->write_array(data, len);
+  }
+
+  virtual int available() const {
+    return this->uart_device_->available();
+  }
+  
+ protected:
+  uart::UARTDevice* uart_device_;
+};
+
+} //namespace modbus_spy
+} //namespace esphome
+
+#endif // ESP32_ARDUINO_UART_INTERFACE_H_
---
+++ components/modbus_spy/uart_interface.h
@@ -0,0 +1,23 @@
+#ifndef UART_INTERFACE_H_
+#define UART_INTERFACE_H_
+
+#ifdef UNIT_TEST
+#include <test_includes.h>
+#else
+#include "esphome/core/datatypes.h"
+#endif // UNIT_TEST
+
+namespace esphome {
+namespace modbus_spy {
+
+class IUartInterface {
+ public:
+  virtual bool read_byte(uint8_t* data) = 0;
+  virtual bool read_array(uint8_t* data, size_t len) = 0;
+  virtual int available() const = 0;
+};
+
+} //namespace modbus_spy
+} //namespace esphome
+
+#endif // UART_INTERFACE_H_
