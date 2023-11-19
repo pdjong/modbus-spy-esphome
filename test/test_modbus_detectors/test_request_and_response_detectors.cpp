5bc8beaef37b0bdbd3d5c4591254f1089765fe82
Paul de Jong <paul.m.de.jong@gmail.com>
2023-11-19 12:00:15 +0100

    Move sniffer test to separate folder

--- test/test_modbus_detectors/test_request_and_response_detectors.cpp
+++ test/test_modbus_detectors/test_request_and_response_detectors.cpp
@@ -593,50 +593,6 @@ void test_modbus_response_detector_response_function_6() {
   TEST_ASSERT_EQUAL_UINT8(0x76, actual_data[3]);
 }
 
-// What to test?
-// - combination of actual request- and response detector, using fake uart interface
-// - combination of matching and non-matching results of detectors, using fake detectors
-
-void test_modbus_sniffer_with_actual_detectors_good() {
-  // Arrange
-  FakeUartInterface fake_uart_interface;
-  uint8_t fake_data[] = { 0x02, 0x03, 0x04, 0xAF, 0x00, 0x01, 0xB5, 0x28, 0x02, 0x03, 0x02, 0x34, 0x56, 0x6A, 0xBA };
-  FakeModbusDataPublisher fake_data_publisher;
-  ModbusSniffer modbus_sniffer(&fake_uart_interface, &fake_data_publisher);
-  bool uart_task_should_stop = false;
-  FakeUartInterfaceTaskArgs args = { 
-    .uart_interface = &fake_uart_interface,
-    .initial_delay_in_ms = 0,
-    .delay_between_bytes_in_us = 573,
-    .data_to_return = fake_data,
-    .len_of_data_to_return = 15,
-    .should_stop = &uart_task_should_stop
-  };
-  TaskHandle_t uart_task_handle { nullptr };
-  xTaskCreatePinnedToCore(fake_uart_interface_task,
-                    "fake_uart_interface_task", // name
-                    30000,                      // stack size (in words)
-                    &args,                      // input params
-                    1,                          // priority
-                    &uart_task_handle,          // Handle, not needed
-                    0                           // core
-  );
-
-  // Act
-  modbus_sniffer.start_sniffing();
-  delay(10);
-  uart_task_should_stop = true;
-  // Delay 5 ms to make sure that the fake uart task is done
-  delay(5);
-
-  // Assert
-  vector<ModbusData*> published_data = fake_data_publisher.get_published_data();
-  TEST_ASSERT_EQUAL_UINT8(1, published_data.size());
-  ModbusData* data = published_data.at(1);
-  TEST_ASSERT_EQUAL_UINT16(0x04AF, data->address);
-  TEST_ASSERT_EQUAL_UINT16(0x3456, data->value);
-}
-
 void generate_crc() {
   uint8_t crc_data[] = { 0x02, 0x03, 0x04, 0xAF, 0x00, 0x01 };
   uint16_t expected_crc = esphome::crc16(crc_data, 6);
@@ -665,9 +621,6 @@ int runUnityTests(void) {
   
   RUN_TEST(test_modbus_response_detector_response_function_1);
   RUN_TEST(test_modbus_response_detector_response_function_6);
-
-  // ModbusSniffer tests
-  RUN_TEST(test_modbus_sniffer_with_actual_detectors_good);
 
   // CRC generation tool :P
   // RUN_TEST(generate_crc);
