f20f0c81626ff5fc3c2a6441db0767d3be052b7f
Paul de Jong <paul.m.de.jong@gmail.com>
2023-11-22 21:33:40 +0100

    Implement ModbusBinarySensor

---
+++ components/modbus_spy/modbus_binary_sensor.cpp
@@ -0,0 +1,30 @@
+#ifndef UNIT_TEST
+
+#ifdef UNIT_TEST
+#include <test_includes.h>
+#else
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/core/datatypes.h"
+#include "esphome/core/log.h"
+#endif // UNIT_TEST
+
+#include "modbus_binary_sensor.h"
+
+namespace esphome {
+namespace modbus_spy {
+
+static const char *TAG = "ModbusBinarySensor";
+
+void ModbusBinarySensor::publish_state(bool state) {
+  ESP_LOGI(TAG, "About to publish state: %d", state);
+  this->sensor_->publish_state(state);
+}
+
+binary_sensor::BinarySensor* ModbusBinarySensor::get_sensor() const {
+  return this->sensor_;
+}
+
+} //namespace modbus_spy
+} //namespace esphome
+
+#endif // UNIT_TEST
---
+++ components/modbus_spy/modbus_binary_sensor.h
@@ -0,0 +1,39 @@
+#ifndef MODBUS_BINARY_SENSOR_H_
+#define MODBUS_BINARY_SENSOR_H_
+
+#ifdef UNIT_TEST
+#include <test_includes.h>
+#else
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/core/datatypes.h"
+#endif // UNIT_TEST
+
+namespace esphome {
+namespace modbus_spy {
+
+class IModbusBinarySensor {
+ public:
+  virtual void publish_state(bool state) = 0;
+};
+
+#ifndef UNIT_TEST
+
+class ModbusBinarySensor : public IModbusBinarySensor {
+ public:
+  ModbusBinarySensor() {
+    this->sensor_ = new binary_sensor::BinarySensor;
+  }
+
+  virtual void publish_state(bool state) override;
+  binary_sensor::BinarySensor* get_sensor() const;
+
+ protected:
+  binary_sensor::BinarySensor* sensor_;
+};
+
+#endif // UNIT_TEST
+
+} //namespace modbus_spy
+} //namespace esphome
+
+#endif // MODBUS_BINARY_SENSOR_H_
